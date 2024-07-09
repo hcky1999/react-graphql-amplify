6ec6f512d19bb338f5b6739e0e8a57fbd0577195
hcky1999 <40015951+hcky1999@users.noreply.github.com>
2024-07-09 15:51:18 -0700

    add <h2> comments

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <h2>Test your website!!!</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
